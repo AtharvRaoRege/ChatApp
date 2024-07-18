c4cfdac882a4874fbeee35c0dabe518034726dde
AtharvRaoRege <atharvrege722@gmail.com>
2024-07-18 22:29:34 +0530

    participants done✅

--- app.js
+++ app.js
@@ -19,22 +19,24 @@ io.on("connection", function (socket) {
   socket.on("nameset", function (namevalue) {
     userids.push(socket.id);
     usernames.push(namevalue);
-
-    io.emit("countofpeople", usernames.length);
+    io.emit("countofpeople", {len:usernames.length,users:usernames});
     socket.emit("namesetdone");
+    io.emit("participants",{users:usernames})
   });
   socket.on("typing",function(){
     let idx = userids.indexOf(socket.id)
-    let name = usernames[idx]
-    socket.broadcast.emit(name);
+    let name = usernames[idx];
+    socket.broadcast.emit("typing",name);
   })
 
+  
   socket.on("disconnect", function () {
     let index = userids.indexOf(socket.id);
     if (index !== -1) {
       userids.splice(index, 1);
       usernames.splice(index, 1);
-      io.emit("countofpeople", usernames.length);
+      io.emit("countofpeople", {len:usernames.length,users:usernames});
+      io.emit("participants",{users:usernames})
     }
   });
 });
@@ -42,7 +44,7 @@ io.on("connection", function (socket) {
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("chat1");
+  res.render("chat");
 });
 
 server.listen(3000);
